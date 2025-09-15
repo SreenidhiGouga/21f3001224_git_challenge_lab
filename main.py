b104ea3fe931fd1a146101512751c6414e9795cd
SreenidhiGouga <168708506+SreenidhiGouga@users.noreply.github.com>
2025-09-15 10:56:43 +0530

    Added main python module

---
+++ main.py
@@ -0,0 +1,12 @@
+import sum
+import difference
+
+def main():
+    a = 10
+    b = 5
+
+    print(f"The sum of {a} and {b} is: {sum.sum(a, b)}")
+    print(f"The difference between {a} and {b} is: {difference.difference(a, b)}")
+
+if __name__ == "__main__":
+    main()
